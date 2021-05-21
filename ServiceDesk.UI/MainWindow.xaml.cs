6821767a330d9af5b57386e319df537fbd7c20db
Юрий Шпорхун <yuripclive@live.ru>
2021-05-21 07:49:26 +0300

    Add project files.

---
+++ ServiceDesk.UI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SeviceDesk.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
